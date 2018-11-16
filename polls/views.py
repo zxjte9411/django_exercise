246d0fb3d3688be5c6b5ed93ea8ef8d67d9f793f
zxjte9411 <a5852241@gmail.com>
2018-11-16 12:41:49 +0800

    update views.py and add some functionals

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import Http404
 # Create your views here.
-
 from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {'latest_question_list': latest_question_list}
+    # return render(request, 'polls/index.html', context)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    
 
-def hellowWorld(request):
-    return HttpResponse("hellowWorld!")
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    return HttpResponse("You're looking at question {}.".format(question_id))
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
