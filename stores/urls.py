8209eee00933c7c2b51657d9e4f9cda4a5d3530a
zxjte9411 <a5852241@gmail.com>
2018-11-29 02:54:05 +0800

    設計 detail 頁面程式碼與設定

--- stores/urls.py
+++ stores/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from . import views 
+from . import views
 
 app_name = 'stores'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('store/', views.store_list, name='store_list'),
+    path('store/<pk>', views.store_detail, name='store_detail'),
 ]
--- stores/views.py
+++ stores/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from .models import Store
 
 
@@ -10,3 +10,11 @@ def home(request):
 def store_list(request):
     stores = Store.objects.all()
     return render(request, 'store_list.html', {'stores': stores})
+
+
+def store_detail(request, pk):
+    try:
+        store = Store.objects.get(pk=pk)
+    except Store.DoesNotExist:
+        raise Http404
+    return render(request, 'store_detail.html', {'store': store})
