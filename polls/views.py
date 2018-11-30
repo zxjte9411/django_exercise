086d89f9e6a170f44ca7f1ac8538833a01115ea0
zxjte9411 <a5852241@gmail.com>
2018-12-01 00:28:46 +0800

    刪除多餘的 code

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
 from django.urls import reverse
 from django.views import generic
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.utils import timezone
 
 from .models import Choice, Question
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -18,13 +18,16 @@ class IndexView(generic.ListView):
         """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
--- stores/admin.py
+++ stores/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from .models import Store, MenuItem
-# Register your models here.
 
-
-
-# @admin.register(MenuItem)
-# class MenuItemAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'price']
 
 class MenuItemInline(admin.TabularInline):
     model = MenuItem
     extra = 1
+
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
