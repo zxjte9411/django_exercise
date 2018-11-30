6a5c26e26edb769a71475d464e0d7045c53ff75b
zxjte9411 <a5852241@gmail.com>
2018-12-01 01:26:12 +0800

    get_absolute_url

--- stores/models.py
+++ stores/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Store(models.Model):
 
     name = models.CharField(max_length=20)
     notes = models.TextField(blank=True, default='')
+
+    def get_absolute_url(self):
+        return reverse('stores:store_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name
