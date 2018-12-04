bfabc14aa2163b918f1f938f4424b257b7e8e99b
zxjte9411 <a5852241@gmail.com>
2018-12-04 13:00:32 +0800

    form 表單樣式更 bootstrap & 使用  crispy_forms

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'stores',
     'base',
     'polls.apps.PollsConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- stores/views.py
+++ stores/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.http import Http404
 from .models import Store
-from django.forms.models import modelform_factory
+from .forms import StoreForm
+# from django.forms.models import modelform_factory
 
 def store_list(request):
     stores = Store.objects.all()
@@ -16,14 +17,15 @@ def store_detail(request, pk):
     return render(request, 'stores/store_detail.html', {'store': store})
 
 def store_create(request):
-    StoreForm = modelform_factory(Store, fields=('name', 'notes',))
+    # StoreForm = modelform_factory(Store, fields=('name', 'notes',))
     if request.method == 'POST':
         form = StoreForm(request.POST)
+        # form = StoreForm(request.POST, submit_title='建立')
         if form.is_valid():
             store = form.save()
             return redirect(store.get_absolute_url())
     else:
-        form = StoreForm()
+        form = StoreForm(submit_title='建立')
     return render(request, 'stores/store_create.html', {'form': form})
 
 def store_update(request, pk):
@@ -31,14 +33,16 @@ def store_update(request, pk):
         store = Store.objects.get(pk=pk)
     except Store.DoesNotExist:
         raise Http404
-    StoreForm = modelform_factory(Store, fields=('name', 'notes'))
+    # StoreForm = modelform_factory(Store, fields=('name', 'notes'))
     if request.method == 'POST':
-        form = StoreForm(request.POST, instance=store)
+        # form = StoreForm(request.POST, instance=store)
+        form = StoreForm(request.POST, instance=store, submit_title='更新')
         if form.is_valid():
             store = form.save()
             return redirect(store.get_absolute_url())
     else:
-        form = StoreForm(instance=store)
+        # form = StoreForm(instance=store)
+        form = StoreForm(instance=store, submit_title='更新')
     return render(request, 'stores/store_update.html', {
         'form': form, 'store': store,
     })
