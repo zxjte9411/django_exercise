7a2156505cafa60538066855ef8bee71fa277b24
zxjte9411 <a5852241@gmail.com>
2018-12-12 00:24:08 +0800

    可更新 store 裡面的品項

--- stores/forms.py
+++ stores/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from .models import Store
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django.forms.models import inlineformset_factory
+
+from .models import Store
+from .models import MenuItem
+
 
 class StoreForm(forms.ModelForm):
     class Meta:
@@ -11,5 +15,18 @@ class StoreForm(forms.ModelForm):
     def __init__(self, *args, submit_title='Submit', **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.helper.disable_csrf = True     # 已經有 CSRF token，不需要重複產生。
         if submit_title:
             self.helper.add_input(Submit('submit', submit_title))
+
+
+BaseMenuItemFormSet = inlineformset_factory(
+    parent_model=Store, model=MenuItem, fields=('name', 'price',), extra=1,)
+
+
+class MenuItemFormSet(BaseMenuItemFormSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False        # 我們要自己包。
+        self.helper.disable_csrf = True     # StoreForm 已經有 CSRF token，不需要重複產生。
--- stores/views.py
+++ stores/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
+from django.shortcuts import render
 from django.urls import reverse
 from django.http import Http404
 from django.http import HttpResponseForbidden
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
+
+from django.forms.models import inlineformset_factory
 # from django.forms.models import modelform_factory
 
 from .models import Store
-from .forms import StoreForm
+from .models import MenuItem
+from .forms import StoreForm, MenuItemFormSet
 from events.forms import EventForm
+
 
 def store_list(request):
     stores = Store.objects.all()
@@ -32,10 +37,8 @@ def store_detail(request, pk):
 
 
 def store_create(request):
-    # StoreForm = modelform_factory(Store, fields=('name', 'notes',))
     if request.method == 'POST':
         form = StoreForm(request.POST)
-        # form = StoreForm(request.POST, submit_title='建立')
         if form.is_valid():
             store = form.save(commit=False)
             if request.user.is_authenticated:
@@ -52,17 +55,27 @@ def store_update(request, pk):
         store = Store.objects.get(pk=pk)
     except Store.DoesNotExist:
         raise Http404
-    # StoreForm = modelform_factory(Store, fields=('name', 'notes'))
+
     if request.method == 'POST':
-        # form = StoreForm(request.POST, instance=store)
         form = StoreForm(request.POST, instance=store, submit_title='更新')
-        if form.is_valid():
+        # 用 post data 建立 formset，並在其（與 store form）合法時儲存
+        menu_item_formset = MenuItemFormSet(request.POST, instance=store)
+        if form.is_valid() and menu_item_formset.is_valid():
             store = form.save()
+            menu_item_formset.save()
             return redirect(store.get_absolute_url())
     else:
-        # form = StoreForm(instance=store)
-        form = StoreForm(instance=store, submit_title='更新')
-    return render(request, 'stores/store_update.html', {'form': form, 'store': store, })
+        form = StoreForm(instance=store, submit_title=None)
+        form.helper.form_tag = False
+        menu_item_formset = MenuItemFormSet(instance=store)
+    # 需要多指定一個 parent_model 參數，Django 才知道要用哪一個 foreign key 建立一對多關聯。
+    # 如果有不止一個 foreign key 指向同一個 model,inlineformset_factory 可以接受一個叫 fk_name 的參數，讓你指定欄位名稱。
+    # MenuItemFormSet = inlineformset_factory(
+    #     parent_model=Store, model=MenuItem, fields=('name', 'price',), extra=3)
+        menu_item_formset = MenuItemFormSet(instance=store)
+    return render(request, 'stores/store_update.html', {
+        'form': form, 'store': store, 'menu_item_formset': menu_item_formset,
+    })
 
 
 @login_required
